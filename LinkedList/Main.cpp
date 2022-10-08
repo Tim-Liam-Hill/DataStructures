dff46f8962080f3a599cb01686ec81021481535e
u17112592 <u17112592@tuks.co.za>
2022-10-08 16:42:21 +0200

    Saving things now while I work with makefile in case I accidentally delete something

---
+++ LinkedList/Main.cpp
@@ -0,0 +1,10 @@
+#include "LinkedList.h"
+
+using namespace std;
+
+int main(){
+
+    LinkedList list();
+    cout<<7283<<endl;
+    return 0;
+}
---
+++ LinkedList/README.md
@@ -0,0 +1,3 @@
+
+Makefile needed trickery to put obj files in seperate directory
+
